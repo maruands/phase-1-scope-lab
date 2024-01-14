48277af8d8a1ec00325ec706e5af5ce2d8495fab
maruands <maruands@gmail.com>
2024-01-14 23:15:45 +0300

    fuctions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "andrew";
+function upperCaseCustomerName()
+{
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer()
+{
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer()
+{
+    return bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer()
+{
+    return leastFavoriteCustomer = "kimaru";
+}
